3c402ce3546335071f64a7cda37f0d3328ca0c65
M.MOHAMED SARIFA <mohamedsarifa2007@gmail.com>
2026-08-18 11:20:25 +0530

    Implement basic TCP client for integer transmission

---
+++ Experiment-05-Socket-Stimulation/prg1client.c
@@ -0,0 +1,76 @@
+#include "header.h"
+
+int main (int argc, char* argv[ ])
+{
+    int s;
+    int n;
+    char* servName;
+    int servPort;
+    int numCount;
+    int integers[100];
+    int recvBuffer[100];
+    int bytesToRecv;
+    int totalBytesRecv = 0;
+    char* ptr;
+    struct sockaddr_in serverAddr;
+    if (argc < 3)
+    {
+        printf ("Error: Server IP and Port are required!\n");
+        exit (1);
+    }
+
+    servName = argv[1];
+    servPort = atoi (argv[2]);
+
+    printf ("Enter number of integers: ");
+    scanf ("%d", &numCount);
+
+    printf ("Enter %d integers: ", numCount);
+    for (int i = 0; i < numCount; i++)
+    {
+        scanf ("%d", &integers[i]);
+    }
+
+    memset (&serverAddr, 0, sizeof(serverAddr));
+    serverAddr.sin_family = AF_INET;
+    inet_pton (AF_INET, servName, &serverAddr.sin_addr);
+    serverAddr.sin_port = htons (servPort);
+
+    if ((s = socket (PF_INET, SOCK_STREAM, 0)) < 0)
+    {
+        perror ("Error: socket creation failed!");
+        exit (1);
+    }
+
+    if (connect (s, (struct sockaddr*)&serverAddr, sizeof(serverAddr)) < 0)
+    {
+        perror ("Error: connection failed!");
+        exit (1);
+    }
+
+    send (s, &numCount, sizeof(int), 0);
+    send (s, integers, numCount * sizeof(int), 0);
+
+    recv (s, &numCount, sizeof(int), 0);
+
+    bytesToRecv = numCount * sizeof(int);
+    ptr = (char*)recvBuffer;
+
+    while (bytesToRecv > 0 && (n = recv (s, ptr, bytesToRecv, 0)) > 0)
+    {
+        ptr += n;
+        bytesToRecv -= n;
+        totalBytesRecv += n;
+    }
+
+    printf ("Received integers from server: ");
+    for (int i = 0; i < numCount; i++)
+    {
+        printf ("%d ", recvBuffer[i]);
+    }
+    printf ("\n");
+
+    close (s);
+
+    exit (0);
+}
